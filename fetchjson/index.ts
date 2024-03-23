60d1af6af519602b484b7f5e50ffeab99056c407
Alex <alex.wang@gofreight.com>
2024-03-23 17:07:08 +0800

    add ts interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,19 @@ import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(res => {
-  console.log(res.data)
+  const todo = res.data as Todo
+
+  console.log(`
+    The ID is : ${todo.id}
+    The Title is : ${todo.title}
+    Complete is : ${todo.completed}
+  `)
 })
+
